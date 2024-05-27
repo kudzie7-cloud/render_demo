19a9c54222dd9b6b7a816a189f5187ab9d67df77
kudzie7-cloud <63996687+kudzie7-cloud@users.noreply.github.com>
2024-05-27 07:59:42 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import dash
+from dash import dcc
+from dash import html
+my_app = dash.Dash()
+my_app.layout = html.Div(children= [html.H1(children= 'Hello, Dash!'),
+                                     html.Div(children= '''
+                                              Dash: A web application framework for python.'''),
+                                              dcc.Graph(
+                                              id = 'Example-Graph', 
+                                              figure={
+                                               'data': [
+                                                {'x': [1, 2, 3], 'y': [4, 1, 2],
+                                                 'type': 'bar', 'name': 'SF'},
+                                                 {'x': [1, 2, 3], 'y': [2, 4, 5],
+                                                  'type': 'bar', 'name': 'Montreal'}
+                                               ],
+                                               'layout': {
+                                                'title': 'Dash data Visualisation'
+                                               }
+                                              }
+                                              )
+                                              ])
+if __name__ == '_main_':
+ my_app.run_server(debug = True)
